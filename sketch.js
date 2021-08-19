08572fca05969b14fdf69fefb33311e2f937abce
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-08-18 20:13:29 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(width/2, 80, 10, 10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -28,16 +28,21 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-	box1=createSprite(width/2, 40, 200, 20);
-	box2=createSprite(300, 70, 20, 100)
-	box3=createSprite(500, 70, 20, 100)
+	box1=createSprite(width/2, 650, 200, 20);
+	box2=createSprite(300, 610, 20, 100)
+	box3=createSprite(500, 610, 20, 100)
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
+
+	box1Body = Bodies.rectangle(width/2, 640, 200, 20, {isStatic:true});
+	box2Body = Bodies.rectangle(300, 610, 20, 100, {isStatic:true});
+	box3Body = Bodies.rectangle(500, 610, 20, 100, {isStatic:true});
+	World.add(world, [box1Body, box2Body, box3Body])
 
 	var packageSprite_options ={
 		isStatic: true
@@ -63,11 +68,7 @@ function draw() {
  
 }
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	packageSprite_options ={
-        Matter,Body,setStatic(packageBodyfalse)
-	}
-}
-}
+function keyPressed() { 
+	if (keyCode === DOWN_ARROW) { 
+		Matter.Body.setStatic(packageBody,false) 
+	} }
